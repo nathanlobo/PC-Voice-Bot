a06cedea25ec8d75f3f2d4de72ea626e889efddd
Nathan Lobo <lobonathan2209@gmail.com>
2024-09-17 19:20:41 +0530

    Added Basic chatbot w/o chat history tracking

---
+++ chat_bot.py
@@ -0,0 +1,9 @@
+import google.generativeai as gpt
+
+gpt.configure(api_key='AIzaSyBpx0ESiM3aYaqdQCKEXZL2OSIWy3K_c-Y')
+geminipro = gpt.GenerativeModel('gemini-pro')
+
+while True:
+    prompt = input("You: ")
+    response = geminipro.generate_content(prompt)
+    print(f"Bot: {response.text}")
